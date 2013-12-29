acbee29c00be89f6a7040c650059015cd40ff813
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2013-12-29 14:53:08 -0500

    Define the client functions for communicating with sls over the wire

---
+++ src/sls.h
@@ -0,0 +1,17 @@
+#ifndef __SLS_H__
+#define __SLS_H__ 1
+
+#include<string>
+#include<list>
+
+using namespace std;
+
+bool append(const char *key, string data);
+list<string> lastn(const char *key, int num_entries);
+list<string> all(const char *key);
+list<string> intervaln(const char *key, int start, int end);
+list<string> intervalt(const char *key, unsigned long long start, unsigned long long end);
+string unwrap(const string value);
+unsigned long long check_time(const string value);
+
+#endif
