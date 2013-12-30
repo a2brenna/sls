8bf5f47682d03ea12fcddcdf3233c0942cd84b34
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2013-12-29 21:16:35 -0500

    Switch to proper use of mutable protobof objects

--- src/slsc.cc
+++ src/slsc.cc
@@ -90,12 +90,13 @@ bool append(const char *key, string data){
 }
 
 list<string> *_interval(const char *key, unsigned long long start, unsigned long long end, bool is_time){
-    list<string> *r = (list<string> *)(malloc (sizeof(list<string>)));
+    list<string> *r = new list<string>;
     sls::Request request;
     sls::Range *range = request.mutable_req_range();
-    range->set_start(start);
-    range->set_end(end);
-    range->set_is_time(is_time);
+    request.mutable_req_range()->set_start(start);
+    request.mutable_req_range()->set_start(start);
+    request.mutable_req_range()->set_end(end);
+    request.mutable_req_range()->set_is_time(is_time);
     request.set_key(key);
 
     sls::Response *response = sls_send(request);
