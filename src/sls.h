50b88e3ef93691856e21bf04c01fd3c1963597b8
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2013-12-30 00:24:57 -0500

    Remove intervaln as it makes no sense to have indexed intervals in lists ordered by insertion time with no fixed point of reference

--- src/sls.h
+++ src/sls.h
@@ -9,7 +9,6 @@ using namespace std;
 bool append(const char *key, string data);
 list<string> *lastn(const char *key, int num_entries);
 list<string> *all(const char *key);
-list<string> *intervaln(const char *key, unsigned long long start, unsigned long long end);
 list<string> *intervalt(const char *key, unsigned long long start, unsigned long long end);
 string unwrap(const string value);
 unsigned long long check_time(const string value);
