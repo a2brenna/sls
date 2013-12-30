a865839b0a2d8764faf95ce0cb2f73c499b2db5a
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2013-12-30 13:56:03 -0500

    Copypasta for millisecond resolution timestamps

--- src/test_client.cc
+++ src/test_client.cc
@@ -1,7 +1,18 @@
 #include "sls.h"
 #include <iostream>
+#include <sys/time.h>
 
 using namespace std;
+unsigned long long hires_time(){
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    unsigned long long millisecondsSinceEpoch =
+    (unsigned long long)(tv.tv_sec) * 1000 +
+    (unsigned long long)(tv.tv_usec) / 1000;
+
+    return millisecondsSinceEpoch;
+}
 
 int main(int argc, char *argv[]){
     cerr << "Test Client Started... " << endl;
@@ -13,6 +24,13 @@ int main(int argc, char *argv[]){
     append(test_key.c_str(), "2");
 
     list<string> *r = all(test_key.c_str());
+    cerr << "Got: " << r->size() << endl;
+
+    r = lastn(test_key.c_str(), 6);
+    cerr << "Got: " << r->size() << endl;
+   
+    unsigned long long time = hires_time();
+    r = intervalt(test_key.c_str(), time - 60000, time);
     cerr << "Got: " << r->size() << endl;
     return 0;
 }
