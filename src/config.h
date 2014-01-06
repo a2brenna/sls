f32cde2543954f9be340b7d4f9efe62c2e163749
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2014-01-05 20:50:54 -0500

    More reasonable paging out parameters

--- src/config.h
+++ src/config.h
@@ -1,8 +1,8 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__ 1
 
-int cache_min = 60;//3600 * 24;
-int cache_max = 180;//3600 * 36;
+int cache_min = 3600 * 24;
+int cache_max = 3600 * 36;
 string disk_dir = "/pool/sls/";
 const char *port = "6998";
 
