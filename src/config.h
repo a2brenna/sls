d7eea701d1a10fccdea7a6c6b69ba7d5ad158a25
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2014-03-04 19:46:55 -0500

    Smaller caches

--- src/config.h
+++ src/config.h
@@ -1,8 +1,8 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__ 1
 
-unsigned int cache_min = 3600 * 24;
-unsigned int cache_max = 3600 * 36;
+unsigned int cache_min = 3600 * 2;
+unsigned int cache_max = 3600 * 4;
 string disk_dir = "/pool/sls/";
 const char *port = "6998";
 
