817440304af9ae5e9b37b2fb14a77fd8269d963d
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2014-03-11 14:10:33 +0000

    Fix missing string header

--- src/config.h
+++ src/config.h
@@ -1,6 +1,10 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__ 1
 
+#include <string>
+
+using namespace std;
+
 unsigned int cache_min = 3600 * 2;
 unsigned int cache_max = 3600 * 4;
 string disk_dir = "/pool/sls/";
