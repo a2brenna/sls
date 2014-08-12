b6d29c002796e65e95803fd546a45919d9d9ff35
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2014-08-12 14:36:24 -0400

    Define interface for Client class

---
+++ src/client.h
@@ -0,0 +1,24 @@
+#ifndef __SLS_CLIENT_H__
+#define __SLS_CLIENT_H__
+
+namespace sls{
+
+    Address *global_server;
+
+    class Client{
+        private:
+
+        public:
+            Client();
+            Client(Address *server);
+            ~Client();
+
+            bool append(const char *key, std::string data);
+            std::list<sls::Value> *lastn(const char *key, unsigned long long num_entries);
+            std::list<sls::Value> *all(const char *key);
+            std::list<sls::Value> *intervalt(const char *key, unsigned long long start, unsigned long long end);
+            unsigned long long check_time(sls::Value value);
+    };
+}
+
+#endif
