efaa3c61024b9efcbce737c79d7fbc9d4452ef77
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2014-08-12 14:38:08 -0400

    Move definition of sls::Server to its own file

--- src/sls.h
+++ src/sls.h
@@ -14,30 +14,7 @@ namespace sls{
             std::string msg;
             SLS_Error(std::string message);
     };
-    class Server{
-        public:
-            Server(std::string dd, unsigned long min, unsigned long max);
-            ~Server();
-            void handle(int sockfd);
-            void handle_next_request();
-            void sync();
-        private:
-            std::map<std::string, std::list<sls::Value> > cache;
-            std::map<std::string, std::mutex> locks;
-            std::mutex incoming_lock;
-            std::stack<int> incoming;
-            sls::Value wrap(std::string payload);
-            void _page_out(std::string key, unsigned int skip);
-            void _file_lookup(std::string key, std::string filename, sls::Archive *archive);
-            void file_lookup(std::string key, std::string filename, std::list<sls::Value> *r);
-            std::string next_lookup(std::string key, std::string filename);
-            unsigned long long pick_time(const std::list<sls::Value> &d, unsigned long long start, unsigned long long end, std::list<sls::Value> *result);
-            unsigned long long pick(const std::list<sls::Value> &d, unsigned long long current, unsigned long long start, unsigned long long end, std::list<sls::Value> *result);
-            void _lookup(int client_sock, sls::Request *request);
-            unsigned long cache_min;
-            unsigned long cache_max;
-            std::string disk_dir;
-    };
+
     void set_local_sls(bool new_val);
     bool append(const char *key, std::string data);
     std::list<sls::Value> *lastn(const char *key, unsigned long long num_entries);
