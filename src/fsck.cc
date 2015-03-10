6d51ff1afa75c84fb992b56127c367c9c2822c36
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-03-09 21:50:17 -0400

    Implement a correcting fsck in C++

--- src/fsck.cc
+++ src/fsck.cc
@@ -3,12 +3,20 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <deque>
+#include <algorithm>
 #include <hgutil/files.h>
 #include <boost/program_options.hpp>
+#include <stdio.h>
+#include <fstream>
 #include "sls.pb.h"
 
 std::string CONFIG_ROOT_DIR = "/pool/sls";
 bool CONFIG_FIX = false;
+
+size_t total_discards = 0;
+size_t fix = 0;
+size_t head_fix = 0;
 
 namespace po = boost::program_options;
 
@@ -41,6 +49,7 @@ void config(int argc, char *argv[]){
 
 std::map< std::string, std::pair<unsigned long long, unsigned long long> > build_file_index( const std::set<std::string> &chunk_files ){
     std::map< std::string, std::pair<unsigned long long, unsigned long long> > index;
+    size_t discarded = 0;
     for (const auto &c: chunk_files){
         const std::string chunk = readfile(c);
         sls::Archive arch;
@@ -52,25 +61,13 @@ std::map< std::string, std::pair<unsigned long long, unsigned long long> > build
             const unsigned long long earliest_timestamp = arch.values(num_values - 1).time();
             const unsigned long long latest_timestamp = arch.values(0).time();
             index[c] = std::pair<unsigned long long, unsigned long long>(earliest_timestamp, latest_timestamp);
+        }
+        else{
+            discarded++;
         }
     }
-    return index;
-}
-
-std::map< std::string, std::pair<unsigned long long, unsigned long long> > build_index( const std::set<std::string> &chunk_files ){
-    std::map< std::string, std::pair<unsigned long long, unsigned long long> > index;
-    for (const auto &c: chunk_files){
-        const std::string chunk = readfile(c);
-        sls::Archive arch;
-        arch.ParseFromString(chunk);
-
-        const auto num_values = arch.values_size();
-
-        if(num_values > 0){
-            const unsigned long long earliest_timestamp = arch.values(num_values - 1).time();
-            const unsigned long long latest_timestamp = arch.values(0).time();
-            index[c] = std::pair<unsigned long long, unsigned long long>(earliest_timestamp, latest_timestamp);
-        }
+    if(discarded > 0){
+        total_discards += discarded;
     }
     return index;
 }
@@ -111,23 +108,13 @@ std::string get_next_chunk( const std::map< std::string, std::pair< unsigned lon
                 highest_stamp = c.second.second;
                 first_chunk = c.first;
             }
-            else{
-                std::cerr << "current_stamp " << c.second.second
-                    << " highest_stamp " << highest_stamp
-                    << " max_stamp " << max_stamp
-                    << std::endl;
-            }
         }
-    }
-    else{
-        std::cerr << "Index is empty" << std::endl;
     }
     return first_chunk;
 }
 
 std::pair<unsigned long long, unsigned long long> get_bounds(const std::string &chunk_file){
     try{
-        std::cerr << "Reading chunk_file " << chunk_file << std::endl;
         const std::string chunk = readfile(chunk_file);
         sls::Archive arch;
         arch.ParseFromString(chunk);
@@ -139,28 +126,65 @@ std::pair<unsigned long long, unsigned long long> get_bounds(const std::string &
             const unsigned long long latest_timestamp = arch.values(0).time();
             return std::pair<unsigned long long, unsigned long long>(earliest_timestamp, latest_timestamp);
         }
-        else{
-            std::cerr << "Got no values!" << std::endl;
-        }
     }
     catch(...){
-        std::cerr << "Caught exception" << std::endl;
+        throw;
     }
     return std::pair<unsigned long long, unsigned long long>(0,0);
 }
 
+std::string strip_to_last(const std::string &foo, const char limit){
+    size_t posn = foo.rfind(limit) + 1;
+    std::string result = foo;
+    result.erase(0,posn);
+    return result;
+}
+
 std::string reset_head(const std::map< std::string, std::pair< unsigned long long, unsigned long long> > &file_index, const std::string &head_path){
     std::string head_chunk_path = get_first_chunk(file_index);
-    size_t slash_position = head_chunk_path.rfind('/') + 1;
-    std::string stripped_path = head_chunk_path;
-    stripped_path.erase(0, slash_position);
-    std::cout << head_chunk_path << " disconnected" << std::endl;
+    std::string stripped_path = strip_to_last(head_chunk_path, '/');
     if(CONFIG_FIX){
         remove(head_path.c_str());
         symlink(stripped_path.c_str(), head_path.c_str());
         std::cout << head_path << " -> " << stripped_path << std::endl;
+        head_fix++;
     }
     return head_chunk_path;
+}
+
+std::vector<std::string> generate_chain( const std::map< std::string, std::pair< unsigned long long, unsigned long long> > &index){
+    std::vector< std::pair<std::pair<unsigned long long, unsigned long long>, std::string >> chain;
+    for(const auto &i: index){
+        chain.push_back(std::pair< std::pair<unsigned long long, unsigned long long>, std::string >(i.second, i.first));
+    }
+
+    std::sort(chain.begin(), chain.end());
+    std::reverse(chain.begin(), chain.end());
+
+    std::vector<std::string> result;
+    for(const auto &c: chain){
+        result.push_back(c.second);
+    }
+    return result;
+}
+
+std::vector<std::string> traverse_chain( const std::string &root, const std::string &head_chunk){
+    std::vector<std::string> result;
+    std::string current_file = root + head_chunk;
+    for(;;){
+        try{
+            result.push_back(current_file);
+            const auto foo = get_next_archive(current_file);
+            if(foo == ""){
+                break;
+            }
+            current_file = root + foo;
+        }
+        catch(...){
+            break;
+        }
+    }
+    return result;
 }
 
 int main(int argc, char *argv[]){
@@ -202,8 +226,6 @@ int main(int argc, char *argv[]){
             std::cout << f.first << " " << f.second.first << " " << f.second.second << std::endl;
         }
         */
-
-        std::cout << "Indexing: " << file_index.size() << std::endl;
 
         if(file_index.size() == 0){
             std::cerr << "No valid data: " << k << std::endl;
@@ -247,9 +269,51 @@ int main(int argc, char *argv[]){
             std::cout << "CAUGHT EXCEPTION CANONICALIZTING " << head_path << std::endl;
         }
 
-        unsigned long long last_good_timestamp = get_bounds(key_path + "/" + current_chunk).first;
-        std::cout << "Head oldest: " << last_good_timestamp << std::endl;
+
+        const auto traversed_chain = traverse_chain(key_path + "/", current_chunk);
+
+        std::deque<std::string> index_chain;
+        for(const auto &c: generate_chain(file_index)){
+            index_chain.push_back(c);
+        }
+
+        while(index_chain.size() > 1){
+            std::string current = index_chain.front();
+            index_chain.pop_front();
+
+            const std::string next_archive = get_next_archive(current);
+            const std::string reported_next_archive = key_path + "/" + next_archive;
+            if(reported_next_archive != index_chain.front()){
+
+                std::string chunk = readfile(current);
+                sls::Archive arch;
+                arch.ParseFromString(chunk);
+                std::cout << "OLD " <<  arch.next_archive() << std::endl;
+                std::string new_next_archive = strip_to_last(index_chain.front(), '/');
+                std::cout << "NEW_NEXT " << new_next_archive << std::endl;
+                arch.set_next_archive(new_next_archive);
+                std::cout << "NEW " <<  arch.next_archive() << std::endl;
+                std::string new_chunk;
+                arch.SerializeToString(&new_chunk);
+
+                if(CONFIG_FIX){
+                    std::cout << "Removing: " << current << std::endl;
+                    std::ofstream file(current, std::ios_base::trunc);
+                    file << new_chunk;
+                    file.close();
+
+                    fix++;
+                }
+                else{
+                    std::cout << "Would replace " << current << std::endl;
+                }
+            }
+        }
+
     }
+    std::cout << "Total Discarded: " << total_discards << std::endl;
+    std::cout << "Fixed: " << fix << std::endl;
+    std::cout << "Head Fixed: " << head_fix << std::endl;
 
     return 0;
 };
