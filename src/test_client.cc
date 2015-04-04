7e4e93e2ea3f5f488acc7ec21d2a4c5421b1bab2
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-04-04 16:56:34 -0400

    Implement a test_client

--- src/test_client.cc
+++ src/test_client.cc
@@ -15,8 +15,8 @@ namespace po = boost::program_options;
 std::string unix_domain_file;
 
 const std::string key_alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-size_t CONFIG_MAX_KEYS = 100;
-size_t CONFIG_MAX_ELEMENTS = 1000;
+size_t CONFIG_MAX_KEYS = 1000;
+size_t CONFIG_MAX_ELEMENTS = 100000;
 
 void get_config(int argc, char* argv[]){
     po::options_description desc("Options");
@@ -38,7 +38,8 @@ int main(int argc, char* argv[]){
     std::cerr << "Test Client Started... " << std::endl;
     get_config(argc, argv);
 
-    sls::global_server = new Remote_UDS(unix_domain_file);
+    sls::global_server = new smpl::Remote_UDS(unix_domain_file);
+    sls::append("TEST", "VALUE");
 
     auto r = std::minstd_rand(std::chrono::high_resolution_clock::now().time_since_epoch().count());
     std::map<std::string, std::vector<std::string>> test_data;
@@ -63,6 +64,8 @@ int main(int argc, char* argv[]){
 
     }
 
+    const auto start_time = std::chrono::high_resolution_clock::now();
+
     for(const auto &k: test_data){
         const std::string key = k.first;
         const std::vector<std::string> data = k.second;
@@ -72,8 +75,12 @@ int main(int argc, char* argv[]){
         }
 
     }
+    const auto end_time = std::chrono::high_resolution_clock::now();
 
-    std::cerr << "Test Complete" << std::endl;
+    std::cerr << "TEST_COMPLETE"
+        << " nanos " << (end_time - start_time).count()
+        << " elements " << total_elements
+        << std::endl;
 
     return 0;
 }
