14349ef4161084181feec17d977c44c606a94608
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-04-04 20:54:56 -0400

    Const references where possible

--- src/sls.h
+++ src/sls.h
@@ -13,12 +13,12 @@ namespace sls{
 
     extern smpl::Remote_Address *global_server;
 
-    bool append(const char *key, std::string data);
-    std::deque<sls::Value> *lastn(const char *key, unsigned long long num_entries);
+    bool append(const char *key, const std::string &data);
+    std::deque<sls::Value> *lastn(const char *key, const unsigned long long &num_entries);
     std::deque<sls::Value> *all(const char *key);
-    std::deque<sls::Value> *intervalt(const char *key, unsigned long long start, unsigned long long end);
-    std::string unwrap(sls::Value value);
-    unsigned long long check_time(sls::Value value);
+    std::deque<sls::Value> *intervalt(const char *key, const unsigned long long start, const unsigned long long &end);
+    std::string unwrap(const sls::Value &value);
+    unsigned long long check_time(const sls::Value &value);
 
 }
 
--- src/slsc.cc
+++ src/slsc.cc
@@ -8,13 +8,13 @@
 
 namespace sls{
 
-bool append(const char *key, std::string data){
+bool append(const char *key, const std::string &data){
     Client c(global_server);
     return c.append(key, data);
 }
 
 
-std::deque<sls::Value> *lastn(const char *key, unsigned long long num_entries){
+std::deque<sls::Value> *lastn(const char *key, const unsigned long long &num_entries){
     Client c(global_server);
     return c.lastn(key, num_entries);
 }
@@ -24,15 +24,15 @@ std::deque<sls::Value> *all(const char *key){
     return c.all(key);
 }
 
-std::deque<sls::Value> *intervalt(const char *key, unsigned long long start, unsigned long long end){
+std::deque<sls::Value> *intervalt(const char *key, const unsigned long long &start, const unsigned long long &end){
     Client c(global_server);
     return c.intervalt(key, start, end);
 }
 
-std::string unwrap(sls::Value value){
+std::string unwrap(const sls::Value &value){
     return value.data();
 }
-unsigned long long check_time(sls::Value value){
+unsigned long long check_time(const sls::Value &value){
     return value.time();
 }
 
