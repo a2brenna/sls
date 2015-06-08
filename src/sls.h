4d5817e4f3c1d39a29d427124c2962d01f696785
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-06-08 15:38:10 -0400

    Check pre-conditions with assertions and document them in header

--- src/sls.h
+++ src/sls.h
@@ -18,6 +18,9 @@ namespace sls{
 
     /* Append data to the list indicated by key at some time (in milliseconds)
      * between the time this function is called and the time it returns.
+     *
+     * key: Any std::string such that key.empty() = false
+     * data: Any string
      */
     bool append(const std::string &key, const std::string &data);
 
@@ -25,18 +28,27 @@ namespace sls{
      * in chronological order.  If the list for key contains less than
      * num_entries, the entire list is returned. If the list for key is empty,
      * or num_entries is 0, a deque of size 0 is returned.
+     *
+     * key: Any std::string such that key.empty() = false
+     * num_entries: >= 0
      */
     std::shared_ptr< std::deque<sls::Value> > lastn(const std::string &key, const unsigned long long &num_entries);
 
     /* Returns a deque contains all the entries in the list for key in
      * chronological order.  If the list is empty or no list exists, a deque
      * of size 0 is returned.
+     *
+     * key: Any std::string such that key.empty() = false
      */
     std::shared_ptr< std::deque<sls::Value> > all(const std::string &key);
 
     /* Returns a deque of all the values in the list for key between time
      * index start and end, inclusive.  start and end must be milliseconds
      * since epoch.
+     *
+     * key: Any std::string such that key.empty() = false
+     * start: 0 <= end
+     * end: 0 >= start
      */
     std::shared_ptr< std::deque<sls::Value> > intervalt(const std::string &key, const unsigned long long &start, const unsigned long long &end);
 
--- src/slsc.cc
+++ src/slsc.cc
@@ -2,32 +2,40 @@
 #include <string>
 
 #include "client.h"
-
 #include "sls.h"
 #include "sls.pb.h"
 
 namespace sls{
 
 bool append(const std::string &key, const std::string &data){
-    assert(key.size() > 0);
+    assert(!key.empty());
+
     Client c(global_server);
     return c.append(key, data);
 }
 
 std::shared_ptr< std::deque<sls::Value> > lastn(const std::string &key, const unsigned long long &num_entries){
-    assert(key.size() > 0);
+    assert(!key.empty());
+
+    if( num_entries == 0){
+        return std::shared_ptr<std::deque<sls::Value>>(new std::deque<sls::Value>());
+    }
+
     Client c(global_server);
     return c.lastn(key, num_entries);
 }
 
 std::shared_ptr< std::deque<sls::Value> > all(const std::string &key){
-    assert(key.size() > 0);
+    assert(!key.empty());
+
     Client c(global_server);
     return c.all(key);
 }
 
 std::shared_ptr< std::deque<sls::Value> > intervalt(const std::string &key, const unsigned long long &start, const unsigned long long &end){
-    assert(key.size() > 0);
+    assert(!key.empty());
+    assert(start <= end);
+
     Client c(global_server);
     return c.intervalt(key, start, end);
 }
@@ -35,6 +43,7 @@ std::shared_ptr< std::deque<sls::Value> > intervalt(const std::string &key, cons
 std::string unwrap(const sls::Value &value){
     return value.data();
 }
+
 unsigned long long check_time(const sls::Value &value){
     return value.time();
 }
