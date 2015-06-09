3037d7af3dd7eb2c8cbfa0ecab50c573ffee4948
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-06-09 17:01:27 -0400

    Document Disk/Wire format

--- src/serialize.h
+++ src/serialize.h
@@ -4,6 +4,20 @@
 #include <vector>
 #include <string>
 
+/* DESCRIPTION DISK/WIRE FORMAT */
+/*
+ * Each file or datastream shall contain 1 or more Records in order of ascending value T.
+ * Each Record shall contain a uint64_t (T) representing milliseconds since Epoch, followed by
+ * A uint64_t (L) representing the length in bytes of the following Datagram, followed by
+ * L bytes representing the Data associated with time T.
+ *
+ * File := Record [Record]
+ * Record := T L Datagram
+ * T := uint64_t (milliseconds since Epoch)
+ * L := uint64_t (length of Datagram
+ * Datagram := [Char]
+ */
+
 std::vector<std::pair<uint64_t, std::string>> de_serialize(const std::string &raw);
 
 #endif
