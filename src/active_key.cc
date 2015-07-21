05628ef6980144299d13f11cb5d46deec658aa2b
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-07-20 20:16:52 -0400

    Fix bounds on lastn lookup

--- src/active_key.cc
+++ src/active_key.cc
@@ -170,10 +170,11 @@ std::string Active_Key::_index_lookup(const size_t &start, const size_t &end){
 std::string Active_Key::last_lookup( const size_t &max_values){
     std::unique_lock<std::mutex> l(_lock);
     const size_t total_elements = _num_elements + _start_pos;
-    const size_t upper_bound = total_elements + 1;
-
-    const int lower_bound_index = upper_bound - max_values;
+    //The - 1 here is because the bounds on these index lookups are inclusive
+    const size_t upper_bound = total_elements - 1;
+    const int lower_bound_index = upper_bound - max_values + 1;
     const size_t lower_bound = std::max(0, lower_bound_index);
 
     return _index_lookup(lower_bound, upper_bound);
 }
+
