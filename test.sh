961a99b45588ba23d113477e4127120e950fcab0
Anthony Brennan <a2brenna@csclub.uwaterloo.ca>
2015-08-17 21:15:44 -0400

    Remove deprecated options from test server invocation

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ TEMP_STORE=$(mktemp -d /tmp/sls.XXXX)
 TEMP_SOCKET=$(mktemp /tmp/sls.XXXX)
 rm "$TEMP_SOCKET"
 
-./sls --dir="$TEMP_STORE" --cache_min=100 --cache_max=150 --unix_domain_file="$TEMP_SOCKET" &
+./sls --dir="$TEMP_STORE" --unix_domain_file="$TEMP_SOCKET" &
 SLS_PID=$!
 
 ./test_client --unix_domain_file=/tmp/sls.sock
